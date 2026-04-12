e222b2dfb874736890f281a38bb9fd6a4f955e3f
Licky367 <licky367@gmail.com>
2026-04-12 17:25:41 +0300

    Delete checkFinancialAdmin.js

--- checkFinancialAdmin.js
+++
@@ -1,9 +0,0 @@
-module.exports = function checkFinancialAdmin(req, res, next) {
-  const role = req.session.role;
-
-  if (!role || !["SUPER_ADMIN", "FINANCIAL_ADMIN"].includes(role)) {
-    return res.status(403).send("Access denied");
-  }
-
-  next();
-};
