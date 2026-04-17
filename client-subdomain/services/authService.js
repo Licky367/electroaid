8a8a3f136f46f0174bfdc243dcc36c37307a75bd
Licky367 <licky367@gmail.com>
2026-04-17 16:43:26 +0300

    Update authService.js

--- client-subdomain/services/authService.js
+++ client-subdomain/services/authService.js
@@ -1,42 +1,42 @@
+const { Client, ClientPasswordReset } = require("../models");
+
+// Find client by email
 exports.findByEmail = async (email) => {
-    const [rows] = await db.query(
-        "SELECT * FROM clients WHERE CLIENT_EMAIL=?",
-        [email]
-    );
-    return rows[0];
+    return await Client.findOne({ CLIENT_EMAIL: email });
 };
 
+// Create new client
 exports.createClient = async (data) => {
-    return db.query(`
-        INSERT INTO clients
-        (CLIENT_NAME, CLIENT_EMAIL, CLIENT_PHONE_NUMBER, CLIENT_PROFILE_IMAGE, CLIENT_PASSWORD)
-        VALUES (?, ?, ?, ?, ?)
-    `, [
-        data.CLIENT_NAME,
-        data.CLIENT_EMAIL,
-        data.CLIENT_PHONE_NUMBER,
-        data.imagePath,
-        data.hashedPassword
-    ]);
+    const client = new Client({
+        CLIENT_NAME: data.CLIENT_NAME,
+        CLIENT_EMAIL: data.CLIENT_EMAIL,
+        CLIENT_PHONE_NUMBER: data.CLIENT_PHONE_NUMBER,
+        CLIENT_PROFILE_IMAGE: data.imagePath,
+        CLIENT_PASSWORD: data.hashedPassword
+    });
+
+    return await client.save();
 };
 
-exports.updatePassword = (id, password) => {
-    return db.query(
-        "UPDATE clients SET CLIENT_PASSWORD=? WHERE id=?",
-        [password, id]
+// Update password
+exports.updatePassword = async (id, password) => {
+    return await Client.findByIdAndUpdate(
+        id, // ⚠️ must be MongoDB _id
+        { CLIENT_PASSWORD: password },
+        { new: true }
     );
 };
 
-exports.saveResetToken = (clientId, token, expires) => {
-    return db.query(`
-        INSERT INTO client_password_resets (clientId, token, expiresAt)
-        VALUES (?, ?, ?)
-    `, [clientId, token, expires]);
+// Save reset token
+exports.saveResetToken = async (clientId, token, expires) => {
+    return await ClientPasswordReset.create({
+        clientId,
+        token,
+        expiresAt: expires
+    });
 };
 
-exports.deleteResetToken = (clientId) => {
-    return db.query(
-        "DELETE FROM client_password_resets WHERE clientId=?",
-        [clientId]
-    );
+// Delete reset token
+exports.deleteResetToken = async (clientId) => {
+    return await ClientPasswordReset.deleteOne({ clientId });
 };
