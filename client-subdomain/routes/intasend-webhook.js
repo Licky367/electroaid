07b2197a6a56cdd2e1f4fb08752b9984944c6214
Licky367 <licky367@gmail.com>
2026-04-17 18:41:04 +0300

    Update intasend-webhook.js

--- client-subdomain/routes/intasend-webhook.js
+++ client-subdomain/routes/intasend-webhook.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { Payment, Assignment } = require("../models");
+const { Payment, Assignment } = require("../../models");
 
 /* ============================= */
 /* CONSTANTS */
