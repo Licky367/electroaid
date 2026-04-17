9558ffa69fc7d23e0579439f23f5672c32edd3d3
Licky367 <licky367@gmail.com>
2026-04-17 18:11:19 +0300

    Update passwordResetHelper.js

--- client-subdomain/helpers/passwordResetHelper.js
+++ client-subdomain/helpers/passwordResetHelper.js
@@ -1,7 +1,8 @@
+const { ClientReset } = require("../models");
+
 exports.getValidToken = async (token) => {
-    const [rows] = await db.query(
-        "SELECT * FROM client_password_resets WHERE token=? AND expiresAt > NOW()",
-        [token]
-    );
-    return rows[0];
+    return await ClientReset.findOne({
+        token,
+        expiresAt: { $gt: new Date() }
+    });
 };
