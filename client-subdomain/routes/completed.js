9f5c7381c96a1372c73bee22ddc05a466dded22a
Licky367 <licky367@gmail.com>
2026-04-17 18:38:15 +0300

    Update completed.js

--- client-subdomain/routes/completed.js
+++ client-subdomain/routes/completed.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { Assignment, Submission } = require("../models");
+const { Assignment, Submission } = require("../../models");
 
 /* ============================= */
 /* AUTH MIDDLEWARE */
